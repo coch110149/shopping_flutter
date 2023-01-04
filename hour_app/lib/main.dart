a4af2e9b990cc4b8b7de44a52e1b60e91e768f35
clint <clinton.edward.cochrane@gmail.com>
2023-01-03 20:34:12 -0600

    add firebase

--- hour_app/lib/main.dart
+++ hour_app/lib/main.dart
@@ -3,8 +3,13 @@ import 'package:hour_app/add_form_page.dart';
 import 'package:hour_app/home_page.dart';
 import 'package:hour_app/list_page.dart';
 import 'package:hour_app/meal.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const HourApp());
 }
 
